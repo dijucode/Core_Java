b1f3a1f07954e292b743e8cf049ce1c5f500c6b3
dijucode <itsdiju@gmail.com>
2024-03-12 01:36:46 +0530

    DemoBufferReader Explaination Code

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+class MyTask implements Runnable {
+    private int taskId;
+
+    public MyTask(int id) {
+        this.taskId = id;
+    }
+
+    public void run() {
+        System.out.println("Task " + taskId + " is executing...");
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        ExecutorService executor = Executors.newFixedThreadPool(200);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        for (int i = 0; i < 10; i++) {
+            Runnable task = new MyTask(i);
+            executor.execute(task);
         }
+
+        executor.shutdown(); // Shutdown the executor when tasks are completed
     }
 }
---
+++ src/getter_setter.java
@@ -0,0 +1,12 @@
+public class getter_setter {
+    static int age;
+    static String name;
+
+    public static void main(String[] args){
+        System.out.println(age);
+        System.out.println(name);
+
+
+    }
+
+}
