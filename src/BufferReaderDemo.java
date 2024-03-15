4b0c466f8da7b5cfad8c71462091b8329ee805e0
dijucode <itsdiju@gmail.com>
2024-03-15 17:58:31 +0530

    Try-Catch

---
+++ src/BufferReaderDemo.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class BufferReaderDemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+//        InputStreamReader isr = new InputStreamReader(System.in);
+//        BufferedReader brd = new BufferedReader(sc);
+
+
+    }
+}
---
+++ src/ExceptioonHanlingDemo.java
@@ -0,0 +1,7 @@
+public class ExceptioonHanlingDemo {
+    public static void main(String[] args) throws Exception {
+        int i = 10/0;
+        System.out.println(i);
+        System.out.println("Main method executed");
+    }
+}
