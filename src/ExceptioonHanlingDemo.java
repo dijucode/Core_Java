c43249e6220373ee877164daf0756d3debf5d5d4
dijucode <itsdiju@gmail.com>
2024-03-30 02:21:41 +0530

    Collections Demo

--- src/ExceptioonHanlingDemo.java
+++ src/ExceptioonHanlingDemo.java
@@ -1,6 +1,6 @@
 public class ExceptioonHanlingDemo {
     public static void main(String[] args) throws Exception {
-        int i = 10/0;
+        int i = 10;
         System.out.println(i);
         System.out.println("Main method executed");
     }
